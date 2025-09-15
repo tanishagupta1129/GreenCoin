d0bd5378fcf7e7b4eba6426c13147042c6ca95dd
tanishagupta1129 <tanishagupta12345678@gmail.com>
2025-09-15 18:38:54 +0530

    Deploy GreenCoin

--- green_coins_project/settings.py
+++ green_coins_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ua+lqhvb7sqjh$*l3@dtzuvc#(kw0p6wm1_kqf$!-*ak%=3$#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['GreenCoins.onrender.com']
 
 
 # Application definition
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'green_coins_project.urls'
@@ -119,6 +121,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR / 'mainapp/static',
 ]
+
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
